39fc71dd03f1c3230e6d147c4a51952f5091db39
Jeremy Meyer <jjman505@gmail.com>
2014-10-19 05:39:39 -0400

    dat build

--- app.js
+++ app.js
@@ -71,10 +71,11 @@ app.use(function(err, req, res, next) {
     });
 });
 
-var server = app.listen(3000, function () {
+var port = process.env.PORT || 3000;
 
-  var host = server.address().address
-  var port = server.address().port
+var server = app.listen(port, function () {
+
+  var host = server.address().address;
 
   console.log('App listening at http://%s:%s', host, port)
 
