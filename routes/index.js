ee51542cec23155f059041bdb15eb6f88271319f
Jeremy Meyer <jjman505@gmail.com>
2014-10-19 05:55:24 -0400

    admin access

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('landing.html');
 });
 
 router.get('/admin', function(req, res) {
