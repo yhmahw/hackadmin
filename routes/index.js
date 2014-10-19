c75e47c8d4a443c7209056322d0a9ea4d5e39ee8
Jeremy Meyer <jjman505@gmail.com>
2014-10-19 11:31:40 -0400

    Yo

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,6 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('landing.html');
-});
-
-router.get('/admin', function(req, res) {
   res.render('admin/index', { name: "Boilermake" });
 });
 
